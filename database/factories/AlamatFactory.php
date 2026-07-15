e8f0fe54a12dcd404df1d2acb7383b6af69c799e
wijayy <mertawijaya77@gmail.com>
2026-07-15 21:44:05 +0800

    change alamat factory

--- database/factories/AlamatFactory.php
+++ database/factories/AlamatFactory.php
@@ -20,18 +20,17 @@ class AlamatFactory extends Factory
         return [
             'nama' => fake()->name(),
             'phone' => fake()->phoneNumber(),
-            'postal_code' => '80225',
+            'postal_code' => '80226',
             'alamat' => "Jalan " . fake()->streetName() . ", RT " . fake()->randomDigit() . "/RW " . fake()->randomDigit() . ", Desa Panjer",
             'user_id' => \App\Models\User::factory(),
             'province' => "BALI",
             'regency' => "DENPASAR",
             'district' => "DENPASAR SELATAN",
             'village' => 'PANJER',
-            'province_id' => 0,
-            'regency_id' => 0,
-            'district_id' => 0,
-            'village_id' => 0,
-
+            'province_id' => 15,
+            'regency_id' => 201,
+            'district_id' => 2175,
+            'village_id' => 26041,
         ];
     }
 }
